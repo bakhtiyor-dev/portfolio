1a42571ecce3bf7a7dae88f909d1b0d7864abe64
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2023-04-04 10:19:39 +0500

    some fixes

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "Stoman Portfolio",
+    title: "Portfolio",
     htmlAttrs: {
       lang: "en",
     },
